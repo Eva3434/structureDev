50e153315ab340b56ce48de7381a55fbc322cc6e
Eva3434 <eva.mendez34000@gmail.com>
2023-01-24 14:21:11 +0100

    dossier 5

--- 5_Exercices_En_Cours_Formation/JavaScript/tp2.js
+++
@@ -1,6 +0,0 @@
-console.log("Tp numero2");
-
-var nom = "Mendez"; 
-var prenom = "Eva"; 
-console.log(nom +""+ prenom); // Output de base
-console.log(`${nom} ${nom}`); //Gabarit littéraux
--- 5_Exercices_En_Cours_Formation/ReactJS/mon_projet/src/component/Voiture.js
+++
@@ -1,6 +0,0 @@
-
-function Voiture(i){
-    return <div>Je suis une voiture {i.marque} {i.nom}</div>
-}
-
-export default Voiture; 
